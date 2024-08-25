cd3e48105770d38698262d6675fdabae4703166c
woutbosteels <wout@bosteels.eu>
2024-08-25 16:35:43 +0200

    Enable logs in python

--- thermometer/thermometer-mock.py
+++ thermometer/thermometer-mock.py
@@ -4,6 +4,18 @@ import random
 import os
 import json
 
+import logging
+from sys import stdout
+
+# Define logger
+logger = logging.getLogger('mylogger')
+
+logger.setLevel(logging.DEBUG)
+logFormatter = logging.Formatter \
+    ("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
+consoleHandler = logging.StreamHandler(stdout)
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
 
 # get env variables set in docker compose
 building = os.getenv("BUILDING", "home")
@@ -15,8 +27,12 @@ temperature = 20.0 + random.uniform(-5, 5) # Starting temperature
 topic = building + "/" + room + "/temperature"
 client = "thermometer-" + building + "-" + room
 
+logger.info("connecting to broker ...")
+
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client)
 client.connect(broker, port)
+
+logger.info("connected to broker")
 
 deviation = 0
 i = 0
@@ -31,8 +47,10 @@ while True:
         "room": room,
         "celsius": round(temperature, 2)
     }
+    json_payload = json.dumps(payload)
 
-    client.publish(topic, json.dumps(payload))
+    client.publish(topic, json_payload)
+    logger.info(f"published {json_payload}")
     i += 1
     if i % 50 == 0:
         deviation = random.uniform(-0.05, 0.05)
